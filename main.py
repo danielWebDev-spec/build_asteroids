4a99867607d67526ba1e41565fb2e9f881215124
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2025-08-19 13:43:37 -0700

    Draw Player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
@@ -8,16 +8,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # create player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # draw
         screen.fill("black")
+        player.draw(screen)  # draw after fill, before flip
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
