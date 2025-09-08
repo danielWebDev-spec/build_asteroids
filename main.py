0632687eb29c88df16c24c72820990d2482327b3
danielWebDev-spec <danielwebdeveloper220@gmail.com>
2025-09-08 09:24:12 -0700

    Groups

--- main.py
+++ main.py
@@ -6,21 +6,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
     # create player in the middle of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
 
         # draw
         screen.fill("black")
-        player.draw(screen)  # draw after fill, before flip
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
